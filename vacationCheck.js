5385482413b2d5968a39e05430c611f08e92cb4e
김동현 <function_dh@stunitas.com>
2019-09-16 12:08:51 +0900

    process argv 내용 추가

--- vacationCheck.js
+++ vacationCheck.js
@@ -1,11 +1,11 @@
 function vacation(){
-	let x = prompt("휴가를 입력해주세요", "");  // 입력값을 string으로 받음
+	var x = process.argv[2]
 	let y = Number(x)
 
 	if(isNaN(y) === false){  // 숫자만 출력되게 NaN 체크
-		y >= 5 ? alert('휴가 합격입니다') : alert('휴가 불합격입니다.');  
+		y >= 5 ? console.log('휴가 합격입니다') : console.log('휴가 불합격입니다');  
 	}else{
-		alert('숫자를 입력해주세요')
+		console.log('숫자를 입력해주세요')
 	}
 }
 
